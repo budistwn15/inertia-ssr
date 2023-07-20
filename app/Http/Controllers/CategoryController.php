40f18c880ca7a9244e0f176b19983aed85848732
Budi Setiawan <budi.setiawan112001@gmail.com>
2023-07-20 22:41:04 +0700

    fast paginate

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ArticleItemResource;
+use App\Models\Article;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function show(Category $category)
     {
+        $articles = Article::query()
+            ->select('title','slug','user_id','teaser','created_at','id')
+            ->with(['tags' => fn($tag) => $tag->select('name','slug')])
+            ->where('category_id',$category->id)
+            ->fastPaginate();
         return inertia('Categories/Show',[
-            'category' => $category
+            'category' => $category,
+            'articles' => ArticleItemResource::collection($articles),
         ]);
     }
 }
--- resources/js/Pages/Categories/Show.jsx
+++ resources/js/Pages/Categories/Show.jsx
@@ -1,8 +1,13 @@
 import {Head} from "@inertiajs/react";
 import App from "@/Layouts/App.jsx";
 import Header from "@/Components/Header.jsx";
+import Container from "@/Components/Container.jsx";
+import Grid from "@/Components/Grid.jsx";
+import ArticleBlock from "@/Components/ArticleBlock.jsx";
+import Pagination from "@/Components/Pagination.jsx";
 
-export default function Show({category}){
+export default function Show({category, ...props}) {
+    const {data: articles, meta, links} = props.articles;
     return (
         <div>
             <Head title={category.name}/>
@@ -12,6 +17,21 @@ export default function Show({category}){
                     This page will show you the articles about {category.name}
                 </Header.Subtitle>
             </Header>
+
+            <Container>
+                {articles.length ?
+                    <>
+                        <Grid>
+                            {articles.map((article) =>
+                                <ArticleBlock key={article.slug} article={article}>
+
+                                </ArticleBlock>
+                            )}
+                        </Grid>
+                        <Pagination {...{meta, links}}/>
+                    </>
+                    : <p>No articles yet...</p>}
+            </Container>
         </div>
     )
 }
